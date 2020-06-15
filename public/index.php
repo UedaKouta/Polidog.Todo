e59727e2f040395af92e46719deb0921e094d164
kotaueda <k.u.okome3214@gmail.com>
2020-06-15 15:55:16 +0900

    add autoload

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-error_log("[". date('Y-m-d H:i:s') . dirname(__DIR__). "にてゲット\n" , 3, "/var/app/current/log/debug.log");
+//error_log("[". date('Y-m-d H:i:s') . dirname(__DIR__). "にてゲット\n" , 3, "/var/app/current/log/debug.log");
 
 require dirname(__DIR__) . '/autoload.php';
 exit((require dirname(__DIR__) . '/bootstrap.php')(PHP_SAPI === 'cli-server' ? 'html-app' : 'prod-html-app'));
